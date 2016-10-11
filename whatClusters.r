1795547430e871737d1973535c0f4ab229aafe3e
Elizabeth Purdom <epurdom@stat.berkeley.edu>
2016-10-11 12:55:41 -0700

    move to R package

--- whatClusters.r
+++
@@ -1,7 +0,0 @@
-whatClusters <- function(id, label = "What Clusters Text") {
-  ns <- NS(id)
-  tagList(
-    h4("Display what clusters:"),
-    helpText("Would you like to display a current summary of the internal cluster experiment object?")
-  )
-}
